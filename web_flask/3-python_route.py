adc3ae0dbadd19727f550ce6fb4369aff5d249ee
prosper <mprosp3r@gmail.com>
2023-10-20 06:42:17 -0700

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" this module we will be dealing with intro to Flask """
+from flask import render_template, request, Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello(name=None):
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbhb(name=None):
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    text = text.replace('_', ' ')
+    return f'C {text}'
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+def python(text='is cool'):
+    text = text.replace('_', ' ')
+    return f'Python {text}'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
