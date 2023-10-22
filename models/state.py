a6a479121744784aeded39c43d38a806a899ed96
prosper <mprosp3r@gmail.com>
2023-10-22 14:46:03 -0700

    update of codes in file

--- models/state.py
+++ models/state.py
@@ -2,17 +2,20 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel
 from models import storage
+from os import getenv
 
 
 class State(BaseModel):
     """ State class """
     name = ""
-#    if storage_type != 'db':
- #       @property
-  #      def cities(self):
-   #         """Get method to return list of cities"""
-    #        cities_list = []
-     #       for city in list(storage.all(City).values()):
-      #          if city.state_id == self.id:
-       #             cities_list.append(city)
-        #    return cities_list
+
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """Get method to return list of cities"""
+            cities_list = []
+            for city in list(storage.all(City).values()):
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
